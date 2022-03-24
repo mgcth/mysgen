bf569e3c9486ec52afbf3212ffeabc5602d860cf
Mladen Gibanica <mgcth@users.noreply.github.com>
2022-03-24 17:43:08 +0100

    Add integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,36 @@
 """
-Functions to test mysgen.
+Integration test of mysgen.
 """
-from unittest.mock import patch, mock_open
+import os
+from mysgen.main import main
 
 
-from mysgen.main import init
+CONFIG_FILE = "tests/fixtures/test_config.json"
+known_output = "tests/fixtures/output"
+output = "tests/output"
 
 
+def test_integration_mysgen():
+    """
+    Integration test for mysgen.
+    """
 
+    main(CONFIG_FILE)
+
+    known_files = [
+        os.path.join(path, name)
+        for path, _, files in os.walk(known_output)
+        for name in files
+    ]
+    test_files = [
+        os.path.join(path, name) for path, _, files in os.walk(output) for name in files
+    ]
+
+    for true_f, test_f in zip(known_files, test_files):
+        with open(true_f, "r") as f:
+            true = f.read()
+
+        with open(test_f, "r") as f:
+            test = f.read()
+
+        assert test == true
